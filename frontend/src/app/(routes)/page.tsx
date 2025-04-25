9cf93bd7e488de696a6c4fd8e73ec18bc00f706b
Kim Yehyun <tinyfeather@naver.com>
2025-04-25 12:53:11 +0900

    feat: global.css 기초설정 #S12P31A207-4

---
+++ frontend/src/app/(routes)/page.tsx
@@ -0,0 +1,8 @@
+export default function Main() {
+    return (
+        <div className="bg-primary flex flex-col items-center justify-center h-screen text-white">
+            <h1>Main Page</h1>
+            <p>This is the main page.</p>
+        </div>
+    );
+};
---
+++ frontend/src/app/(routes)/select/page.tsx
@@ -0,0 +1,8 @@
+export default function SelectPage() {
+  return (
+    <div className="bg-amber-900">
+      <h1>Select Page</h1>
+      <p>This is the select page.</p>
+    </div>
+  );
+}
