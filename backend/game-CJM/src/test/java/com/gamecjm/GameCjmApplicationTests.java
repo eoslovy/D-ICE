d243345822b4a13309af3016ec69ba6c879d9616
recreate.kang <recreate.kang@gmail.com>
2025-04-30 15:02:18 +0900

    init: initial infra 설정 추가

---
+++ backend/game-CJM/src/test/java/com/gamecjm/GameCjmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamecjm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameCjmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
