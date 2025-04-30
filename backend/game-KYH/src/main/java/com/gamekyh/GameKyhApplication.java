a9e470366221e4c60d8136860522b15c56784d8f
recreate.kang <recreate.kang@gmail.com>
2025-04-30 15:02:18 +0900

    init: initial infra 설정 추가

---
+++ backend/game-KYH/src/main/java/com/gamekyh/GameKyhApplication.java
@@ -0,0 +1,13 @@
+package com.gamekyh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameKyhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameKyhApplication.class, args);
+	}
+
+}
