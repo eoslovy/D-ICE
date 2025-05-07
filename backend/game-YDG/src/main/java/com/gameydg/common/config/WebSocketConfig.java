e7d6fd08b66b1b1ae9848e47df4dec1f22d52d10
eoslovy <ydg0630@naver.com>
2025-05-07 10:49:03 +0900

    refactor: 웹소켓 공통으로 수정

--- backend/game-YDG/src/main/java/com/gameydg/common/config/WebSocketConfig.java
+++ backend/game-YDG/src/main/java/com/gameydg/common/config/WebSocketConfig.java
@@ -1,11 +1,12 @@
 package com.gameydg.common.config;
 
+import java.util.Map;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-
-import com.gameydg.numberSurvivor.handler.NumberSurvivorWebSocketHandler;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,11 +14,14 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSocket
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketConfigurer {
-	private final NumberSurvivorWebSocketHandler numberSurvivorWebSocketHandler;
+
+	// key: URI, value: WebSocketHandler
+	private final Map<String, WebSocketHandler> handlerMap;
 
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		registry.addHandler(numberSurvivorWebSocketHandler, "/ws/number-survivor")
-			.setAllowedOrigins("*");
+		handlerMap.forEach((path, handler) -> {
+			registry.addHandler(handler, path).setAllowedOrigins("*");
+		});
 	}
 }
