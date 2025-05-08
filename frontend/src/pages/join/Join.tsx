0950ae5c99138678b0bb82ac0914fd5e16c17b4a
차정문 <ckwjdans4182@gmail.com>
2025-05-08 18:02:20 +0900

    feat: QR코드 로직 구현

--- frontend/src/pages/join/Join.tsx
+++ frontend/src/pages/join/Join.tsx
@@ -7,13 +7,22 @@ export default function Join() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (roomCode) {
-      // roomCode를 로컬 스토리지에 저장
-      localStorage.setItem("roomCode", roomCode);
-      console.log(`roomCode ${roomCode} 저장 완료`);
-    }else{
-        console.log("roomCode가 없습니다.");
+    if (!roomCode){
+      console.log("roomCode가 없습니다")
+      return;
     }
+    // roomCode가 6자리 숫자인지 확인
+    const isValidRoomCode = /^\d{6}$/.test(roomCode);
+    if (!isValidRoomCode) {
+      console.log("roomCode가 유효하지 않습니다:", roomCode);
+      return;
+    }
+    console.log('Join useEffect 실행');
+
+    // roomCode를 로컬 스토리지에 저장
+    localStorage.setItem("roomCode", roomCode);
+    console.log(`roomCode ${roomCode} 저장 완료`);
+
     // /lobby로 리다이렉트
     navigate("/lobby", { replace: true });
 
