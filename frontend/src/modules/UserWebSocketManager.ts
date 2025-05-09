8f49e81c198c7d65fb7b4fe3b53cfaabbee2be3e
오지석 <jiseok95@gmail.com>
2025-05-09 14:23:58 +0900

    feat: requestId에 대한 검증이 어렵기 때문에 handleMessage 삭제
    - USER_JOINED_ADMIN, BROADCAST 같은 message들은 직접 보낸 것들이 아니기 때문에 requestId에 대한 검증이 불가함

--- frontend/src/modules/UserWebSocketManager.ts
+++ frontend/src/modules/UserWebSocketManager.ts
@@ -62,71 +62,11 @@ class UserWebSocketManager extends WebSocketManager<UserReceiveTypeMap> {
             return newNickname;
         }
     }
-
-    protected handleMessage(msg: ReceiveMessage): void {
-        if (this.isReceiveMessageWithRequestId(msg)) {
-            const timeoutId = this.pendingRequests.get(msg.requestId);
-            if (timeoutId) {
-                clearTimeout(timeoutId);
-                this.pendingRequests.delete(msg.requestId);
-            } else {
-                this.emit("unknown_requestId", msg.requestId, msg);
-                return;
-            }
-        }
-    }
-
-    // private setUpOnMessage() {
-    //     if (!this.ws) return;
-    //     this.ws.onmessage = (event) => {
-    //         let message: ReceiveMessage;
-    //         try {
-    //             message = JSON.parse(event.data);
-    //             console.log(
-    //                 "[UserWebSocketManager] Message received:",
-    //                 message
-    //             );
-
-    //             // --- Handle Response Validation ---
-    //             if (this.isReceiveMessageWithRequestId(message)) {
-    //                 if (this.pendingRequests.has(message.requestId)) {
-    //                     const timeoutId = this.pendingRequests.get(
-    //                         message.requestId
-    //                     );
-    //                     clearTimeout(timeoutId); // Clear the timeout for this request
-    //                     this.pendingRequests.delete(message.requestId); // Remove from pending requests
-    //                 } else {
-    //                     console.warn(
-    //                         `[WebSocketManager] Received response for unknown requestId: ${message.requestId}`
-    //                     );
-    //                     this.emit(
-    //                         "unknown_requestId",
-    //                         message.requestId,
-    //                         message
-    //                     );
-    //                     return; // Ignore unknown requestId
-    //                 }
-    //             }
-    //             // --- End Response Validation ---
-
-    //             this.emit<keyof UserReceiveTypeMap>(
-    //                 message.type as keyof UserReceiveTypeMap,
-    //                 message
-    //             );
-    //         } catch (e) {
-    //             console.error(
-    //                 "[WebSocketManager] Failed to parse message:",
-    //                 event.data,
-    //                 e
-    //             );
-    //             this.emit("raw_message", event.data);
-    //         }
-    //     };
-    // }
 }
 
 const userWebSocketManager = new UserWebSocketManager();
 
 export default userWebSocketManager;
+
 
 
