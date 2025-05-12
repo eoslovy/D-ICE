6bf3c9f9bb27803eb422565731b6eed2b997aaab
이승록 <rokja97@naver.com>
2025-05-12 13:57:10 +0900

    chore: test module input
    - 테스트 모듈 입력 받기

--- frontend/src/modules/TestModules.tsx
+++ frontend/src/modules/TestModules.tsx
@@ -39,7 +39,7 @@ export default function TestModules(props: TestModulesProps) {
   const [rawMessageError, setRawMessageError] = useState<string | null>(null);
 
   // --- State for Test Game Start ---
-  const [testGameTypeInput, setTestGameTypeInput] = useState<string>("Dye");
+  const [testGameTypeInput, setTestGameTypeInput] = useState<string>("Clicker");
 
   // --- POTG Event Listeners ---
   useEffect(() => {
@@ -205,7 +205,7 @@ export default function TestModules(props: TestModulesProps) {
   const testStartGame = () => {
     const testData = {
       type: 'WAIT',
-      gameType: 'Clicker',
+      gameType: testGameTypeInput, // string 게임 타입
       startAt : 1745382394207, // long epoch ms 게임 시작 시각
       duration: 10000, // long ms 게임 지속시간 
       currentMs: 1745382384207, // long epoch ms 현재 시각 유저와 시간 align 용
