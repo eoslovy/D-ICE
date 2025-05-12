89a967b2b12e68a0cbc9f9cfa2633b9b14fe7da9
recreate.kang <recreate.kang@gmail.com>
2025-05-12 17:58:04 +0900

    S12P31A207-94 프론트 게임 제작(그래프) init: 웹소켓 초기설정

---
+++ backend/game-KJH/src/main/java/com/gamekjh/config/WebSocketConfig.java
@@ -0,0 +1,31 @@
+package com.gamekjh.config;
+
+import java.util.Map;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.context.ApplicationContext;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+	private final ApplicationContext applicationContext;
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		Map<String, WebSocketHandler> handlers = applicationContext.getBeansOfType(WebSocketHandler.class);
+
+		handlers.forEach((beanName, handler) -> {
+			String path = resolvePathFromHandler(beanName, handler);
+			registry.addHandler(handler, path).setAllowedOrigins("*");
+		});
+	}
+
+	private String resolvePathFromHandler(String beanName, WebSocketHandler handler) {
+		return "/ws/" + beanName.toLowerCase().replace("websockethandler", "");
+	}
+}
