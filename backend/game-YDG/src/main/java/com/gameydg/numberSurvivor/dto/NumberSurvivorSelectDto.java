298c7d7165e12298501862fa8ab066779f9fc00c
eoslovy <ydg0630@naver.com>
2025-05-12 13:25:16 +0900

    refactor: type 지정

--- backend/game-YDG/src/main/java/com/gameydg/numberSurvivor/dto/NumberSurvivorSelectDto.java
+++ backend/game-YDG/src/main/java/com/gameydg/numberSurvivor/dto/NumberSurvivorSelectDto.java
@@ -8,15 +8,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class NumberSurvivorSelectDto {
-    private String type;
+    private final String type = "NUMBER_SURVIVOR_SELECT";
     private String userId;
     private String roomId;
     private Integer selectedNumber;
-    
-    // 필요한 필드만으로 객체를 생성할 수 있는 생성자 추가
-    public NumberSurvivorSelectDto(String roomId, String userId, Integer selectedNumber) {
-        this.roomId = roomId;
-        this.userId = userId;
-        this.selectedNumber = selectedNumber;
-    }
 }
