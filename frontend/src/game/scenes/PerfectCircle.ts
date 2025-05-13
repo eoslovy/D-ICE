688e9290952886fba4e9d8fe1ec33ebb144d4e22
eoslovy <ydg0630@naver.com>
2025-05-13 10:33:44 +0900

    feat: 원 그리기 게임 팟지 녹화 기능 추가

--- frontend/src/game/scenes/PerfectCircle.ts
+++ frontend/src/game/scenes/PerfectCircle.ts
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import POTGManager from '../../modules/POTGManager';
 
 interface Point {
   x: number;
@@ -17,6 +18,7 @@ export class PerfectCircle extends Phaser.Scene {
   private guideCenterY!: number;
   private guideRadius!: number;
 
+  
   constructor() {
     super('PerfectCircle');
   }
@@ -61,6 +63,9 @@ export class PerfectCircle extends Phaser.Scene {
       this.drawing = true;
       this.points = [];
       this.graphics.clear();
+  
+      // 원 그리기 시작할 때 녹화 시작
+      this.startRecording();
   
       // ⭐ 시작점 지정
       this.graphics.beginPath();
@@ -167,10 +172,30 @@ export class PerfectCircle extends Phaser.Scene {
   
     // 3초 후 GameOver 씬으로
     this.time.delayedCall(3000, () => {
+      // 녹화 중지 추가
+      this.stopRecording();
+      
       this.scene.start('GameOver', { 
         score: this.score,
         gameType: 'PerfectCircle'
       });
     });
+  }
+
+  // 녹화 시작 함수 수정
+  private async startRecording() {
+    try {
+      const started = await POTGManager.startScreenRecording();
+      if (started) {
+        console.log('[PerfectCircle] 녹화가 시작되었습니다!');
+      }
+    } catch (error) {
+      console.error('[PerfectCircle] 녹화 시작 중 오류:', error);
+    }
+  }
+
+  // 녹화 중지 함수 수정
+  private stopRecording() {
+    POTGManager.stopRecording();
   }
 }
